1a27cdc27be03f7838a682d7120f48187b48a486
Husni <husniwfayo@gmail.com>
2024-07-05 22:39:21 +0530

    initial commit

---
+++ aidf-back-end/src/applications/rating.ts
@@ -0,0 +1,46 @@
+import OpenAI from "openai";
+import JobApplication from "../infrastructure/schemas/job-application";
+
+export const generateRating = async (jobApplicationId) => {
+  try {
+    const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+    const jobApplication = await JobApplication.findById(jobApplicationId).populate('job');
+
+    if (!jobApplication) {
+      console.error("Job application not found");
+      return;
+    }
+
+    const content = `Role:${jobApplication.job.title}, User Description:${jobApplication.answers.join(". ")}`;
+
+    const completion = await client.chat.completions.create({
+      messages: [{ role: "user", content }],
+      model: "ft:gpt-3.5-turbo-0125:stemlink:aidrivenproject:9gqqc76N"
+    });
+
+    const strResponse = completion.choices[0].message.content;
+    console.log("Response from model:", strResponse);
+
+    const response = JSON.parse(strResponse);
+
+    if (!response.rate) {
+      console.error("Rating not found in response");
+      return;
+    }
+
+    const updateResult = await JobApplication.findOneAndUpdate(
+      { _id: jobApplicationId },
+      { rating: response.rate },
+      { new: true }  // Return the updated document
+    );
+
+    if (!updateResult) {
+      console.error("Failed to update the job application");
+    } else {
+      console.log("Updated job application:", updateResult);
+    }
+  } catch (error) {
+    console.error("Error in generating rating:", error);
+  }
+};
